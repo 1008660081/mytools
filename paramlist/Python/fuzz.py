9526b0507c337972aec54cf8ca7cda5f1ece1970
root <root@C20231009153002.local>
2024-01-31 21:21:49 +0800

    add

---
+++ paramlist/Python/fuzz.py
@@ -0,0 +1,24 @@
+from urllib import request, parse
+
+# 读取文件中的参数
+with open("params.txt") as f:
+    parameters = [line.strip() for line in f]
+
+# 每组参数的数量
+n = 50
+
+# XSS payload
+xss = "lol123%3cx"
+
+out = ""
+
+for num, parameter in enumerate(parameters, start=1):
+    current_num = (num - 1) % n + 1  # 将 num 的范围调整为 1 到 n
+    out += f"{parameter}={xss}{current_num}&"
+    if num % n == 0:
+        print(out[:-1])
+        out = ""
+
+# 打印剩余的参数
+if out:
+    print(out[:-1])
---
+++ paramlist/deduplicate_urls_1.py
@@ -0,0 +1,26 @@
+from urllib.parse import urlparse
+
+def deduplicate_urls_with_limit(input_file, output_file, limit=1):
+    url_count = {}
+    unique_urls = []
+
+    with open(input_file, 'r') as infile:
+        for line in infile:
+            url = line.strip()
+            path = urlparse(url).path
+
+            # 统计路径出现的次数
+            url_count[path] = url_count.get(path, 0) + 1
+
+            # 判断是否超过限制
+            if url_count[path] <= limit:
+                unique_urls.append(url)
+
+    with open(output_file, 'w') as outfile:
+        for unique_url in unique_urls:
+            outfile.write(unique_url + '\n')
+
+if __name__ == "__main__":
+    input_file = "urls.txt"
+    output_file = "shuf.txt"
+    deduplicate_urls_with_limit(input_file, output_file, limit=1)
